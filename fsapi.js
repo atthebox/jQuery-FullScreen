ee8e5ca4f86ef8d13a0dd507b066a8454bed3e46
James Nash <james@atthebox.com>
2012-04-03 00:06:24 -0700

    Completely rewrote original plugin. Add support for multiple elements. Add several new public methods. Addressed callback issues. Now with examples.

---
+++ fsapi.js
@@ -0,0 +1,194 @@
+/**
+ * @name        jQuery FullScreen API Plugin (fsapi)
+ * @author      James Nash
+ * @version     1.1
+ * @url         https://github.com/atthebox/jQuery-FullScreen
+ * @license     MIT License
+ */
+
+(function($){
+
+  // storage of callbacks
+  // & options for each element
+  var fsConfig   = {};
+  var fsCallback = {};
+
+  // default values for options parameter
+  // of constructor
+  var defaults = {
+    'background'          : '#111',
+    'fullscreen_class'    : 'fs-fullscreen',
+    'fullscreen_children' : false
+  };
+
+  // These helper functions available only to our plugin scope.
+
+  var _eventCallback = function (){
+    var current = $.currentFullScreenDiv;
+    var obj = $('[fskey=' + current + ']');
+    if (current != '') {
+      fsCallback[current](obj, _fullScreenStatus());
+    }
+    if (!_fullScreenStatus()) {
+      $.currentFullScreenDiv = '';
+    }
+  };
+
+  var _randomString = function(length){
+    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+    var randomstring = '';
+    for (var i=0; i<length; i++) {
+      var rnum = Math.floor(Math.random() * chars.length);
+      randomstring += chars.substring(rnum,rnum+1);
+    }
+    return randomstring;
+  };
+
+  var _supportFullScreen = function(){
+    var doc = document.documentElement;
+
+    return  ('requestFullscreen' in doc) ||
+        ('mozRequestFullScreen' in doc && document.mozFullScreenEnabled) ||
+        ('webkitRequestFullScreen' in doc);
+  };
+
+  var _requestFullScreen = function (elem){
+
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    }
+    else if (elem.mozRequestFullScreen) {
+        elem.mozRequestFullScreen();
+    }
+    else if (elem.webkitRequestFullScreen) {
+        elem.webkitRequestFullScreen();
+    }
+  };
+
+  var _fullScreenStatus = function (){
+    return  document.fullscreen ||
+        document.mozFullScreen ||
+        document.webkitIsFullScreen;
+  };
+
+  var _cancelFullScreen = function (){
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    }
+    else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    }
+    else if (document.webkitCancelFullScreen) {
+        document.webkitCancelFullScreen();
+    }
+  };
+
+  // Adding a new test to the jQuery support object
+  $.support.fullscreen = _supportFullScreen();
+
+  $.currentFullScreenDiv = '';
+
+  // public methods
+  var methods = {
+    "init" : function( options, cb ) {
+          if(!$.support.fullscreen){
+              return this;
+          }
+      
+          if (typeof options === 'function')
+          {
+              cb = options;
+              options = undefined;
+          }
+          if (cb == undefined) {
+               cb = function(){};
+          } 
+          
+          $(document).off("fullscreenchange mozfullscreenchange webkitfullscreenchange", _eventCallback);
+          $(document).on("fullscreenchange mozfullscreenchange webkitfullscreenchange", _eventCallback);
+
+          return this.each(function() {
+
+             var $this   = $(this);
+             var key     = _randomString(5);
+             var tmp     = Object.create(defaults);
+            
+             $.extend(tmp, options);
+            
+             fsConfig[key]   = tmp;
+             fsCallback[key] = cb;         
+
+             $this.attr('fskey', key);
+
+          });
+      },
+      "close" : function() {
+          if(_fullScreenStatus()){
+             _cancelFullScreen();
+          }
+          var key = this.attr('fskey');
+          var fs = $('div#fs-modal');
+        
+          this.find("*").removeClass(fsConfig[key].fullscreen_class);
+          this.removeClass(fsConfig[key].fullscreen_class).insertBefore(fs);
+          fs.remove();
+        
+          return this;
+      },
+      "open" : function() {
+          var key = this.attr('fskey');
+          $.currentFullScreenDiv = key;
+
+          var fs = $('<div>', {
+            'id'  : 'fs-modal',
+            'css' : {
+                'overflow'   : 'hidden',
+                'margin'     : '0',
+                'padding'    : '0',        
+                'background' : fsConfig[key].background,
+                'width'      : '100%',
+                'height'     : '100%'
+            }
+          });
+
+          var elem = this;
+          elem.addClass(fsConfig[key].fullscreen_class);
+        
+          if (fsConfig[key].fullscreen_children) {
+            elem.find('*').addClass(fsConfig[key].fullscreen_class);
+          }
+
+          fs.insertBefore(elem);
+          fs.append(elem);
+          _requestFullScreen(fs.get(0));
+
+          return this;
+    },
+    "width" : function() {
+        return screen.width;
+    },
+    "height" : function() {
+        return screen.height;
+    },
+    "isFull" : function() {
+        if (_fullScreenStatus()){
+            return true;
+        }
+        return false;
+    }
+  };
+
+  // Creating the plugin
+  $.fn.fsapi = function(method){
+    // Method calling logic
+    if ( methods[method] ) {
+      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || typeof method == 'function' || !method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' is not a public method of jQuery.fsapi' );
+    }
+
+  };
+
+})(jQuery);
--- readme.md
+++ readme.md
@@ -1,70 +1,112 @@
-# jQuery FullScreen Plugin
+# jQuery FullScreen API Plugin
 
-A jQuery 1.7 plugin that wraps around the *[Full Screen API](https://developer.mozilla.org/en/DOM/Using_full-screen_mode)* and works around various browser differences. Works in FF 10, Chrome and Safari. It is useful for presenting users with an easier to read version of your web pages, or zooming *&lt;canvas&gt;* and *&lt;video&gt;* elements.
+The Fullscreen API is a still experimental feature of HTML5. Currently browser support is limited to Firefox, Chrome and Safari. 
 
-## How to use
 
-Include jquery.fullscreen.js in your page along with version 1.7 of the jQuery library. This gives you the `$('#elem').fullScreen()` method. You can optionally pass an object with properties:
+## Usage Example
 
+More examples are available in the examples directory.
+
+```js
+$(document).ready(function(){
+
+    // options for constructor
+    var options = {
+        "fullscreen_class":"my-fullscreen-class"
+    };
+
+    // instantiate plugin (with a callback)
+    $('div#my-fs-div').fsapi(options, function(elem, open){
+        if (open) {
+            elem.html('div#my-fs-div is fullscreen');
+        } else {
+            elem.html('div#my-fs-div is NOT fullscreen');
+        };
+    });
+
+    // set click event for open button
+    $('button#my-button-open').click(function(){
+        $('div#my-fs-div').fsapi('open');
+    });
+
+    // set click event for close button
+    $('button#my-button-close').click(function(){
+        $('div#my-fs-div').fsapi('close');
+    });
+
+    // hide buttons for unsupported browsers
+    if (!$.support.fullscreen) {
+        $('button#my-button-open').hide();
+        $('button#my-button-close').hide();
+    }
+
+});
+    
+```
+## Constructor Options
 <table>
-	<tr>
-		<th>Property</th>
-		<th>Value</th>
-		<th>Meaning</th>
-	</tr>
+  <tr>
+    <th>Property</th>
+    <th>Default Value</th>
+    <th>Description</th>
+  </tr>
     <tr>
-        <td>background</td>
-        <td>a color hash</td>
-        <td>This is the color that will be used for the background.</td>
+        <td><b>background</b> (string)</td>
+        <td>'#fff'</td>
+        <td>This CSS background value will be applied to the div created and placed behind the fullscreen element.</td>
     </tr>
     <tr>
-        <td>callback</td>
-        <td>a function</td>
-        <td>The callback function will be called on a full screen change event. It has one argument - a boolean indicating whether we are in full screen or not.</td>
+      <td><b>fullscreen_class</b> (string)</td>
+        <td>'fs-fullscreen'</td>
+        <td>This CSS class is added to the element when it is at full screen.</td>
+    </tr>
+    <tr>
+      <td><b>fullscreen_children</b> (bool)</td>
+        <td>false</td>
+        <td>Whether or not to add the <b>full_screen</b> class to child elements.</td>
     </tr>
 </table>
 
-After the plugin makes your element full screen, it will add the `.fullScreen` class on it, so you can easily style it.
+## Public Methods
+<table>
+  <tr>
+    <th>Method</th>
+    <th>Returns</th>
+    <th>Description</th>
+  </tr>
+    <tr>
+        <td><b>$('elem').fsapi([object] options, [function] callback)</b> (constructor)</td>
+        <td>this</td>
+        <td>This CSS background value will be applied to the div created and placed behind the fullscreen element.</td>
+    </tr>
+    <tr>
+        <td><b>$('elem').fsapi('open')</b></td>
+        <td>this</td>
+        <td>Open the element specified by the selector.</td>
+    </tr>
+    <tr>
+        <td><b>$('elem').fsapi('close')</b></td>
+        <td>this</td>
+        <td>Close the element specified by the selector.</td>
+    </tr>
+    <tr>
+        <td><b>$('elem').fsapi('width')</b></td>
+        <td>int</td>
+        <td>Returns the maximum fullscreen width. Equivalent to: screen.width.</td>
+    </tr>
+    <tr>
+        <td><b>$('elem').fsapi('height')</b></td>
+        <td>int</td>
+        <td>Returns the maximum fullscreen height. Equivalent to: screen.height.</td>
+    </tr>
+    <tr>
+        <td><b>$('elem').fsapi('isFull')</b></td>
+        <td>bool</td>
+        <td>Returns true if any element has fullscreen status. Otherwise returns false.</td>
+    </tr>
+</table>
 
-## Example
+## History
 
-```js
-// The plugin sets the $.support.fullscreen flag:
-if($.support.fullscreen){
-	
-	// ...
-	// Show your full screen button here
-	// ...
-	
-	$('#fullScreen').click(function(e){
-	
-		$('#content').fullScreen();
-		
-		// You can also pass a hash with properties:
-		// $('#content').fullScreen({
-		//	'background'	: '#111',
-		//	'callback'		: function(isFullScreen){
-		//		// ...
-		//		// Do some cleaning up here
-		//		// ...
-		//	}
-		// });
-	});
-}
-```
+This code was forked from Martin Angelov's *[jQuery-FullScreen](https://github.com/martinaglv/jQuery-FullScreen)*
 
-You can then apply additional styles to your element. Take the opportunity to increase the font size, hide distractions and make for a better reading experience.
-
-```css
-
-#content.fullScreen{
-	/* Give the element a width and margin:0 auto; to center it. */
-}
-
-```
-
-If you later wish **to cancel the full screen view**, you can do so by calling the `fullScreen()` method again.
-
-## Demo
-
-Go to [Tutorialzine](http://tutorialzine.com/2012/02/enhance-your-website-fullscreen-api/) for a live demo.
